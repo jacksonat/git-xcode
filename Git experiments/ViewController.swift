c62abf1d3d01665741dfe42bdca8ffa9f40dd034
Jackson Taylor <closprieur@gmail.com>
2016-08-21 11:15:12 +1000

    Initial Commit

---
+++ Git experiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git experiments
+//
+//  Created by Jackson Taylor on 21/08/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
